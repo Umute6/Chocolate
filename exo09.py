34671425e6e38a40e20fb5cfaf046b082b8922f8
Sonia Bryon <s.bryon@interface3.be>
2023-04-14 14:37:45 +0200

    bouh

---
+++ exo09.py
@@ -0,0 +1,28 @@
+from random import randint
+
+#sequence_lenght = 3
+
+numbers = range(1, 7)
+
+sequence = []
+
+#if sequence == sequence:
+for i in range(sequence_lenght):
+    i = randint(1, len(numbers) -1)
+    value = numbers[i]
+
+    sequence.append(value)
+
+if i != numbers[i]:
+    for i in range(sequence_lenght):
+        i = randint(1, len(numbers) -1)
+        value = numbers[i]
+
+
+print(sequence)
+
+
+
+
+
+
